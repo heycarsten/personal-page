7287e411b172b4c24bfe28afca598db4f4aacdb0
Carsten Nielsen <heycarsten@gmail.com>
2019-01-06 15:17:15 -0500

    Fix index page metadata

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,9 +1,18 @@
 import Route from '@ember/routing/route';
+import { inject } from '@ember/service';
+import config from 'heycarsten/config/environment';
 
 export default Route.extend({
+  headData: inject(),
+
   model() {
     return {
       year: new Date().getFullYear()
     };
+  },
+
+  // Kinda hacky :( - figure out why ember-meta doesn't use default meta here
+  afterModel() {
+    this.headData.setProperties(config['ember-meta']);
   }
 });
